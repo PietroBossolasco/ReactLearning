c628a7ce7ef121f7a16e3f58eac6bc75c87a5df0
PietroBossolasco <p.bossolasco.1715@vallauri.edu>
2023-02-06 21:01:58 +0100

    Added general functionality to ToDo

--- ToDo/App.js
+++ ToDo/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from './components/header.js'
 import TodoItem from './components/TodoItem.js'
 import AddTodo from './components/addTodo.js'
@@ -7,39 +7,40 @@ import Bottombar from './components/bottombar.js'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
-  const [todos, setToDos] = useState()
+  const [todos, setToDos] = useState(
+    [
+      { text: 'Buy coffee', key: '1' },
+      { text: 'Create an app', key: '2' },
+      { text: 'Play on the switch', key: '3' },
+    ]
+  )
 
-  // [
-  //   { text: 'Buy coffee', key: '1' },
-  //   { text: 'Create an app', key: '2' },
-  //   { text: 'Play on the switch', key: '3' },
-  // ]
 
-  const storeData = async (name, value) => {
-    try {
-      await AsyncStorage.setItem(name, value)
-    } catch (e) {
-      console.error(e);
-    }
-  }
+  // const storeData = async (name, value) => {
+  //   try {
+  //     await AsyncStorage.setItem(name, value)
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // }
   
-  const setStartItem = async () => {
-    try {
-      const value = await AsyncStorage.getItem("todo")
-      console.log("Entrato")
-      let tempvalue = JSON.parse(value)
-      console.log(tempvalue);
-      for(let item of tempvalue){
-        setToDos(tempvalue)
-        console.log(item)
-      }
-      console.log("Uscito")
-    } catch (e) {
-    }
-  }
+  // const setStartItem = async () => {
+  //   try {
+  //     const value = await AsyncStorage.getItem("todo")
+  //     console.log("Entrato")
+  //     let tempvalue = JSON.parse(value)
+  //     console.log(tempvalue);
+  //     for(let item of tempvalue){
+  //       setToDos(tempvalue)
+  //       console.log(item)
+  //     }
+  //     console.log("Uscito")
+  //   } catch (e) {
+  //   }
+  // }
   
-  setStartItem();
-  console.log("Uscito")
+  // setStartItem();
+  // console.log("Uscito")
 
   const pressHandler = (key) => {
     setToDos((prevTodos) => {
@@ -48,38 +49,51 @@ export default function App() {
   }
 
   const submitHandler = (text) => {
-    setToDos((prevTodos) => {
-      let temp = [];
-      temp = prevTodos;
-      let id = (parseInt(temp[temp.length - 1].key) + 1).toString();
-      console.log(id);
-      temp.push({ text: text, key: id });
-      storeData('todo', JSON.stringify(temp));
-      return [
-        ...prevTodos
-      ]
-    })
+    if(text.length > 3){
+      setToDos((prevTodos) => {
+        // let temp = [];
+        // temp = prevTodos;
+        // let id = (parseInt(temp[temp.length - 1].key) + 1).toString();
+        // console.log(id);
+        // temp.push({ text: text, key: id });
+        // storeData('todo', JSON.stringify(temp));
+        return [
+          {text: text, key : (parseInt(prevTodos[prevTodos.length - 1].key) + 1).toString()},
+          ...prevTodos
+        ]
+      })
+    }
+    else{
+      Alert.alert("Opss...", "The length of the todo must be over 3 char", [
+        {text: 'Understood', onPress: () => console.log("Alert closed")}
+      ])
+    }
+
   }
 
   return (
-    <View style={styles.container}>
-      {/* Header */}
-      <Header />
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+    }}>
+      <View style={styles.container}>
+        {/* Header */}
+        <Header />
 
-      <View style={styles.content}>
-        {/* To form */}
-        <AddTodo submitHandler={submitHandler} />
-        <View style={styles.list}>
-          <FlatList
-            data={todos}
-            renderItem={({ item }) => (
-              <TodoItem item={item} pressHandler={pressHandler} />
-            )}
-          />
+        <View style={styles.content}>
+          {/* To form */}
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
         </View>
+        <Bottombar />
       </View>
-      <Bottombar />
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
