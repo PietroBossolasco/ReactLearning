18dd7fa334e31a7c74bc4a9d23703c84efcd45ab
PietroBossolasco <p.bossolasco.1715@vallauri.edu>
2023-02-10 10:07:30 +0100

    Edit TODO item layout

--- ToDo/components/TodoItem.js
+++ ToDo/components/TodoItem.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons'
 
-export default function TodoItem({ item, pressHandler }){
-    return(
+export default function TodoItem({ item, pressHandler }) {
+    return (
         <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={styles.item}>{item.text}</Text>
+            <View style={styles.item}>
+                <MaterialIcons style={styles.icon} name="delete" size={18} color="red" />
+                <Text>{item.text}</Text>
+            </View>
         </TouchableOpacity>
     )
 }
 
 const styles = StyleSheet.create({
-    item : {
-        padding : 16,
-        marginTop : 16,
-        borderColor : "#bbb",
-        borderWidth : 1,
-        borderStyle : "dashed",
-        borderRadius : 10
+    item: {
+        flexDirection : "row",
+        padding: 16,
+        marginTop: 16,
+        borderColor: "#bbb",
+        borderWidth: 1,
+        borderStyle: "dashed",
+        borderRadius: 10
+    },
+    icon : {
+        marginRight : 10
     }
 })
